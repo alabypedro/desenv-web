b5559c7b5c22a60a93c8db0ab9e27fe603aca292
alabypedro <85690152+alabypedro@users.noreply.github.com>
2023-11-29 19:33:14 -0300

    main

--- sistema.php
+++ sistema.php
@@ -48,7 +48,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">SISTEMA</a>
+            <a class="navbar-brand" href="#">Lista dos Clientes</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
